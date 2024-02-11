ee66622ae2d742341f5fa28307193c324dd35b9d
Fela Calana <fela@users.noreply.github.com>
2024-02-11 17:13:52 +0100

    partly fixed inital bigram model

--- main.py
+++ main.py
@@ -1,12 +1,13 @@
 import torch
 import datasets
 
+raw_data = datasets.load_dataset('tiny_shakespeare')
+all_data = {
+    'train': raw_data['train']['text'][0],
+    'test': raw_data['test']['text'][0]
+}
 
-all_data = datasets.load_dataset(name='tiny_shakespear')['train']
-
-print(all_data)
-
-chars = ''.join(set('<placeholder>'))
+chars = ''.join(set(all_data['train']+all_data['test']))
 input_dim = len(chars)
 char_to_i = {c: i for i, c in enumerate(chars)}
 i_to_char = {i: c for i, c in enumerate(chars)}
@@ -15,7 +16,8 @@ print_loss_every_n_steps = 100
 
 
 class Net(torch.nn.Module):
-    def __init__(self):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
         self.logits = torch.nn.Linear(input_dim, input_dim, bias=False)
 
     def forward(self, x):
@@ -49,36 +51,42 @@ def evaluate_split(net: Net, split: str, steps: int=100) -> Net:
     tot = 0
     for _ in range(steps):
         with torch.no_grad():
-            position = torch.Random().randint(0, len(all_data) - context_size - 1)
+            # torch has no attribute Random
+            position = torch.randint(low=0, high=len(data) - context_size - 1, size=(1,)).item()
             string = data[position:position + context_size + 1]
-            x = string[:, :-1]
-            y = string[:, 1:]
+            inp = encode(string)
+            x = inp[:-1]
+            y = inp[1:]
             y_hat = net(x)
             loss = criterion(y_hat, y)
             tot += loss.item()
     print(tot / steps)
-    return net
 
 
 def train(net, steps=10000) -> Net:
     optimizer = torch.optim.Adam(net.parameters(), lr=1e-3)
     criterion = torch.nn.CrossEntropyLoss()
     for i in range(steps):
-        position = torch.Random().randint(0, len(all_data) - context_size - 1)
+        if i % print_loss_every_n_steps == 0:
+            for split in ['train', 'test']:
+                print(f'Loss on {split} at step {i}: ', end='')
+        position = torch.randint(low=0, high=len(all_data['train']) - context_size - 1, size=(1,)).item()
         string = all_data['train'][position:position + context_size + 1]
-        x = string[:, :-1]
-        y = string[:, 1:]
+        inp = encode(string)
+        x = inp[:-1]
+        y = inp[1:]
         y_hat = net(x)
         loss = criterion(y_hat, y)
         optimizer.zero_grad()
         loss.backward()
         optimizer.step()
-        if i % print_loss_every_n_steps == 0:
-            net = evaluate_split(net, 'train')
-            net = evaluate_split(net, 'test')
     return net
 
 
 def main():
     net = train(Net())
     print(net.generate(1000))
+
+
+if __name__ == '__main__':
+    main()
